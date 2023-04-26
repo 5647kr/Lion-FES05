536262fec2232e41763496e47895855caa79df72
5647kr <5647kr@gmail.com>
2023-04-26 23:42:38 +0900

    first commit

--- 04. Assignment/[과제]메모장/메모장.js
+++ 04. Assignment/[과제]메모장/메모장.js
@@ -17,38 +17,71 @@ cancel.addEventListener("click", () => {
   memo.classList.remove("write")
 })
 
+// 로컬스토리지 저장
+
+let memolists = [];
+
+const savememo = () => {
+  localStorage.setItem("memolists", JSON.stringify(memolists));
+};
+
+
 // 저장버튼을 누르면 작성포스트잇은 사라지고 상단부터 순서대로 우측으로 나열된다.
-save.addEventListener("click", () => {
-  memo.style.display = "none";
-  memo.classList.remove("write")
-  if(content.value !== "") {
+const delItem = (event) => {
+  const target = event.target.parentElement.parentElement; // 부모의 부모요소
+
+  memolists = memolists.filter((memoitem) => memoitem.id !== parseInt(target.id));
+  savememo();
+  target.remove();
+};
+
+const addItem = (memoitem) => {
+  if(memoitem.text !== "") {
+    memo.style.display = "none";
+    memo.classList.remove("write")
     const li = document.createElement("li");
-    li.innerHTML = `<div class="memo list"></div>`;
+    const memolist = document.createElement("div");
     ul.appendChild(li);
-    const memolist = document.querySelector(".memo.list");
+    memolist.classList.add("list");
+    li.appendChild(memolist);
     const memocontent = document.createElement("p");
-    memocontent.innerText = content.value;
-    memolist.appendChild(memocontent)
-    document.querySelector(".content").value = "";
+    memocontent.innerText = memoitem.text;
+    memolist.appendChild(memocontent);
+    const button = document.createElement("button");
+    button.classList.add("del")
+    button.innerText = "X";
+    memolist.appendChild(button)
+    button.addEventListener("click", delItem)
+    li.id = memoitem.id
   }
-})
+};
 
+const handler = (event) => {
+  if(memoitem !== "") {
+    alert("내용을 입력하세요!")
+  }
+  memoitem = {
+    id: Date.now(),
+    text: content.value,
+  };
 
+  memolists.push(memoitem)
+  addItem(memoitem);
+  savememo();
 
+  content.value = "";
+};
 
+save.addEventListener("click", handler);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const init = () => {
+  const usermemos = JSON.parse(localStorage.getItem("memolists"));
+  
+  if(usermemos) {
+    usermemos.forEach((memoitem) => {
+      addItem(memoitem);
+    });
+    memolists = usermemos;
+  }
+};
+init();
