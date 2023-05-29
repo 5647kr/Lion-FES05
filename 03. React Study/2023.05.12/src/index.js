8ab4e54b98657b61604986a8145894354f5f7401
5647kr <5647kr@gmail.com>
2023-05-29 20:48:33 +0900

    first commit

--- 03. React Study/2023.05.12/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from "react-dom/client";
-import App from "./1st Class StyledComponent/GlobalStyled";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
---
+++ 03. React Study/2023.05.15 React/src/2nd Class StyledComponent/StyledComponent5.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const ContentOne = styled.div`
+  margin: 40px;
+`;
+
+const ContentTwo = styled.div`
+  color:red;
+`;
+
+const ContentThree = styled(ContentTwo)`
+/* 상속받은 것 */
+  border: 1px solid black;
+`
+
+function App() {
+  return (
+    <div>
+      <ContentOne>hello world</ContentOne>
+      <ContentTwo>hello world</ContentTwo>
+      <ContentThree>hello world</ContentThree>
+    </div>
+  );
+}
+
+export default App;
