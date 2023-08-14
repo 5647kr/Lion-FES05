276331e15572543915c0d7198d47f035526febd2
5647kr <5647kr@gmail.com>
2023-08-14 12:44:11 +0900

    JavaScript 사용방법 & 로또 & 자소서 & JQuery & 기념일

---
+++ JavaScript/2023.03.28 JavaScript/1st]JavaScript 사용방법.js
@@ -0,0 +1,22 @@
+document.write("조코딩 짱짱");
+  // ; 하나의 코드가 끝났다는 의미
+  // : 한줄주석
+  // /**/: 블록주석
+
+// 변수 선언 방법
+// let 변수명 = 값;
+// const 변수명 = 값;
+
+// 데이터의 특징: 자료형
+/* 
+자료형의 종류
+문자열(string): 이름 등등 ("" , ''로 문자를 감싸야한다.)
+숫자(int, float): 정수형(int):30 실수(float):1.2
+불(bool): true, false */
+
+// 자료형의 형태를 아는 방법
+/* typeof 데이터 */
+var name= "엄준식";
+
+document.write(typeof name);
+
