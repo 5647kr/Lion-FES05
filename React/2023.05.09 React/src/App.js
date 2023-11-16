d7abc3f2e23c27e219cb8bbae9bff7335cfd5036
5647kr <5647kr@gmail.com>
2023-11-16 16:30:50 +0900

    React Event & Component

--- React/2023.05.09 React/src/App.js
+++ React/2023.05.09 React/src/App.js
@@ -5,9 +5,9 @@ import ReactEvent1 from './1st Class/ReactEvent1';
 import ReactEvent2 from './2nd Class/ReactEvent2';
 import ReactEvent3 from './3rd Class/ReactEvent3';
 import ComponentList from './3rd Class/ComponentList';
-import BasicHook1 from './4th Class/BasicHook1';
-import BasicHook2 from './5th Class/BasicHook2';
-import Mood from './6th Class/Mood';
+// import BasicHook1 from './4th Class/BasicHook1';
+// import BasicHook2 from './5th Class/BasicHook2';
+// import Mood from './6th Class/Mood';
 
 
 function App() {
@@ -19,9 +19,9 @@ function App() {
         <Route path='/ReactEvent2' element={<ReactEvent2 />} />
         <Route path='/ReactEvent3' element={<ReactEvent3 />} />
         <Route path='/ComponentList' element={<ComponentList />} />
-        <Route path='/BasicHook1' element={<BasicHook1 />} />
+        {/* <Route path='/BasicHook1' element={<BasicHook1 />} />
         <Route path='/BasicHook2' element={<BasicHook2/>} />
-        <Route path='/Mood' element={<Mood />} />
+        <Route path='/Mood' element={<Mood />} /> */}
       </Routes>
     </Router>
   );
--- React/2023.05.09 React/src/Main.jsx
+++ React/2023.05.09 React/src/Main.jsx
@@ -11,9 +11,9 @@ export default function Main() {
       <button onClick={() => navigate('/ReactEvent2')}>2교시</button>
       <button onClick={() => navigate('/ReactEvent3')}>3-1교시</button>
       <button onClick={() => navigate('/ComponentList')}>3-2교시</button>
-      <button onClick={() => navigate('/BasicHook1')}>4교시</button>
+      {/* <button onClick={() => navigate('/BasicHook1')}>4교시</button>
       <button onClick={() => navigate('/BasicHook2')}>5교시</button>
-      <button onClick={() => navigate('/Mood')}>6교시</button>
+      <button onClick={() => navigate('/Mood')}>6교시</button> */}
     </div>
   )
 }
