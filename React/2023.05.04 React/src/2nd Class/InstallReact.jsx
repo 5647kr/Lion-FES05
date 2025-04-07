6bedc8b4812884cebf4f841a57bb7cae0234380f
5647kr <5647kr@gmail.com>
2025-04-07 20:43:37 +0900

    React InstallReact

--- React/2023.05.04 React/src/2nd Class/InstallReact.jsx
+++ React/2023.05.04 React/src/2nd Class/InstallReact.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function InstallReact() {
   return (
-    <div>InstallReact
-      {/* React 프로젝트 설치
-      npx create-react-app 생성할폴더명 --template basic-react */}
+    <div>
+      <h1>React 설치</h1>
+      <p>React 설치 명령어</p>
+      <p>npx create-react-app 생성할폴더명 --template basic-react</p>
     </div>
   )
 }
