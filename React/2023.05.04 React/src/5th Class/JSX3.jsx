79cf68483c2331f68a390551f5af53a79f97296d
5647kr <5647kr@gmail.com>
2025-04-08 08:23:06 +0900

    React JSX

--- React/2023.05.04 React/src/5th Class/JSX3.jsx
+++ React/2023.05.04 React/src/5th Class/JSX3.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 export default function JSX3() {
+  // 예제1
   const name = '라이캣!'
 
+  // 예제2
   const 변수 = 'value';
   function 함수() {
     console.log('함수 출력!')
@@ -13,6 +15,7 @@ export default function JSX3() {
     <div>
       <h1>JSX3</h1>
 
+      {/* 실습1 */}
       {/* 틀린거 고쳐보기 */}
 
       {/* <textarea
@@ -27,11 +30,13 @@ export default function JSX3() {
         style={{backgroundColor: 'blue'}} 
       />
 
+      {/* 예제1 */}
       <h2>{name}</h2>
 
-
+      {/* 예제2 */}
       {100 + 1}
       {'hello' + 'world'}
+      {`${[1, 2, 3]}`}
       {[1, 2, 3].map(x => x**2)}
       {함수()}
       {변수}
@@ -43,20 +48,20 @@ export default function JSX3() {
 }
 
 function Time() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const newDate = new Date();
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const date = newDate.getDate();
 
-  const hour = date.getHours()
-  const min = date.getMinutes();
-  const sec = date.getSeconds();
+  const hour = newDate.getHours();
+  const min = newDate.getMinutes();
+  const sec = newDate.getSeconds();
 
   return (
-    <div style={{backgroundColor : "black", color: "white"}}>
-      <h2 style={{color : "red"}}>년: {year}</h2>
-      <h2>월/일: {month}/{day}</h2>
-      <h2>시간: {hour}시 {min}분 {sec}초</h2>
+    <div style={{backgroundColor: "black"}}>
+      <h2 style={{color: "red"}}>년: {year}</h2>
+      <h2 style={{color: "white"}}>월/일: {month}/{date}</h2>
+      <h2 style={{color: "white"}}>시간: {hour}시 {min}분 {sec}초</h2>
     </div>
   )
 }
