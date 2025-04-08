41a5a28f5f9801d36808d7f36c31d3deff2fa3b0
5647kr <5647kr@gmail.com>
2025-04-08 21:05:22 +0900

    React useState

--- React/2023.05.08 React/src/4th Class/BasicHook1.jsx
+++ React/2023.05.08 React/src/4th Class/BasicHook1.jsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 export default function BasicHook1() {
   return (
     <div>
-      <h1>BasicHook1</h1>
+      <h1>BasicHook: UseState</h1>
 
-      <Resume1 />
-      <Resume2 />
+      <p>변수의 값이 변경될 때마다 페이지를 업데이트 하도록 만들어 주는것이 useState이다.</p>
+      <p>[변수, 변수가 변경할 떄 실행할 함수] = useState(초기값)</p>
+      <UseState1 />
+      <UseState2 />
     </div>
   )
 }
 
-function Resume1() {
+function UseState1() {
   let like = 0;
 
   function clickLike() {
@@ -26,7 +28,7 @@ function Resume1() {
   )
 }
 
-function Resume2() {
+function UseState2() {
   console.log(useState(0))
   const [like, setLike] = useState(0);
   const [txt, setTxt] = useState("hello")
--- React/2023.05.08 React/src/5th Class/BasicHook2.jsx
+++ React/2023.05.08 React/src/5th Class/BasicHook2.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react'
 export default function BasicHook2() {
   return (
     <div>
-      <h1>BasicHook2</h1>
-      <Resume1 />
+      <h1>BasicHook: UseState</h1>
+      <UseState1 />
     </div>
   )
 }
 
-function Resume1() {
+function UseState1() {
   const [like, setLike] = useState(false);
 
   function clickLike() {
