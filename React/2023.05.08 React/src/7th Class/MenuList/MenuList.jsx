e337f0f71cd48732c2b435a71501afbb0fa3e695
5647kr <5647kr@gmail.com>
2025-04-09 10:30:50 +0900

    React 과제 주석 처리

--- React/2023.05.08 React/src/7th Class/MenuList/MenuList.jsx
+++ React/2023.05.08 React/src/7th Class/MenuList/MenuList.jsx
@@ -7,6 +7,7 @@ export default function MenuList(props) {
   return (
     <ul className='container-list'>
       {menus.map((moodEl) => {
+        // mood: 기분의 텍스트, isSelected: 현재 사용자의 기분이 맞는지 전달, onClickEvent: 현재 기분을 변경하는 함수
         return <MenuListItem mood = {moodEl} onClickEvent={props.onItemClick} isSelected={props.mood === moodEl}/>
       })}
     </ul>
