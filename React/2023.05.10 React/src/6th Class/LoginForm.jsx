466da5deff649ab1da252ab675b15636b8c78596
5647kr <5647kr@gmail.com>
2025-04-10 14:59:11 +0900

    React LoginForm

--- React/2023.05.10 React/src/6th Class/LoginForm.jsx
+++ React/2023.05.10 React/src/6th Class/LoginForm.jsx
@@ -1,88 +1,103 @@
 import React, { useState } from 'react'
-import './LoginForm.css'
+import "./LoginForm.css"
 
 export default function LoginForm() {
   const [datas, setDatas] = useState([]);
 
+  console.log(datas)
+
+  //... 전개구문는 배열안에 있는걸 푸는것 data는 추가하는것
   const addData = (data) => {
-    setDatas([...datas, data])//... 전개구문는 배열안에 있는걸 푸는것 
-    //data는 추가하는것
+    setDatas([...datas, data]);
   }
 
   return (
-    <>
-      <ItemCreator datas={datas}/>
+    <div>
+      <h1>LoginForm</h1>
+
       <EventForm addData={addData}/>
-    </>
+      <ItemGenerator datas={datas} />
+    </div>
   )
 }
 
+
 function EventForm({addData}) {
-  const [title, setTitle] = useState('');
-  const [date, setDate] = useState('');
-  const [food, setFood] = useState('짜장면');
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [food, setFood] = useState("짜장면");
 
-  //초기화 기능
-  function resetForm() {
-    setTitle('')
-    setDate('')
-  }
 
-  //제출 기능
-  function submitForm(event) {
-    event.preventDefault();
+  function submitForm(e) {
+    e.preventDefault();
 
-    const formdata = {
-      id: Math.floor(Math.random() * 1000),
+    const formData = {
+      id: Math.floor(Math.random() * 10000),
       title: title,
       date: date,
       food: food
     }
-    console.log(formdata)
+    console.log(formData)
 
-    addData(formdata);
+    addData(formData);
     resetForm();
   }
 
+
+  function resetForm() {
+    setTitle("")
+    setDate("")
+    setFood("짜장면")
+  }
+
   return (
-    <>
+    // input에 value를 추가하면서 reset버튼을 눌러도 초기화 되지 않는다. 그 이유는 value값을 관리하는게 html이 아닌 react로 넘어가서 초기화 되지 않는다.
     <form className='login-form' onSubmit={submitForm}>
       <label>
-        <strong>Login Title: </strong>
-        <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
+        <strong>Event Title: </strong>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
       </label>
       <label>
-        <strong>Login Date: </strong>
-        <input type="date" value={date} onChange={(event) => setDate(event.target.value)}/>
+        <strong>Event Date: </strong>
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
       </label>
       <label>
-        <select onChange={(event) => {setFood(event.target.value)}}>
+        <select onChange={(e) => setFood(e.target.value)}>
           <option value="짜장면">짜장면</option>
           <option value="짬뽕">짬뽕</option>
-          <option value="유산슬">유산슬</option>
+          <option value="탕수육">탕수육</option>
         </select>
       </label>
 
-      <button type='submit'>제출</button>
+      <button type='submit'>제출하기</button>
       <button type='reset' onClick={resetForm}>초기화</button>
     </form>
-    </>
   )
 }
 
-function ItemCreator({datas}) {
-  console.log(datas)
+
+function ItemGenerator({datas}) {
   return (
-      <div>
-        {datas.map((data) => {
-          return (
-            <div key={data.id}>
-              <h2>{data.title}</h2>
-              <time>{data.date}</time>
-              <strong>{data.food}</strong>
-            </div>
-          )
-        })}
-      </div>
-    )
+    <ul>
+      {datas.map((data) => {
+        return (
+          <li key={data.id}>
+            <h2>{data.id}. {data.title}</h2>
+            <time>{data.date}</time>
+            <strong>{data.food}</strong>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
+
+
+
+
+
+
+
+
+
+
