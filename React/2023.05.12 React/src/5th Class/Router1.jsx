284da917a6fae2c6bc90f04f52419f9d57548e1f
5647kr <5647kr@gmail.com>
2025-04-11 16:12:48 +0900

    React Router

--- React/2023.05.12 React/src/5th Class/Router1.jsx
+++ React/2023.05.12 React/src/5th Class/Router1.jsx
@@ -1,21 +1,94 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
 
 export default function Router1() {
   return (
-    <div>
-      <h1>Router1</h1>
-
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="Router1/one" element={<One name="licat" />} />
-        <Route path="Router1/two" element={<Two />} />
-        <Route path="Router1/three" element={<Three />} />
-      </Routes>
-    </div>
+    <main style={{margin: "0 300px"}}>
+      <Example1 />
+    </main>
   );
 }
-// 주소창에 localhost:3000'/one' ''안을 바꾸면서 화면을 바꾼다. 
+
+
+function Example1() {
+  const codeString = `
+    function RouterPractice() {
+      return (
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="Router1/one" element={<One name="licat" />} />
+          <Route path="Router1/two" element={<Two />} />
+          <Route path="Router1/three" element={<Three />} />
+        </Routes>
+      )
+    }
+
+    export function Index() {
+      return <h1>hello world0</h1>;
+    }
+
+    export function One({ name }) {
+      return <h1>{name} world1</h1>;
+    }
+
+    export function Two() {
+      return <h1>hello world2</h1>;
+    }
+
+    export function Three() {
+      return <h1>hello world3</h1>;
+    }
+  `
+  
+  return (
+    <Wrap>
+      <Title>Router</Title>
+        <p>
+          라우터는 URL 주소창을 지켜보며, 변화하면 변화에 알맞는 페이지를 사용자에게 안내해주는 기능이다.<br />
+          라우터에는 BrowserRouter, Routes, Route등이 있다.
+        </p>
+        <ul>
+          <li>
+            BrowserRouter: URL과 URL을 연결한다. 마치 SSR과 같이 URL을 사용할 수 있다.
+          </li>
+          <li>
+            Routes: URL이 변경되면 Routes의 모든 자식요소 Route를 살펴보고 가장 알맞는 것을 매칭한다.
+          </li>
+          <li>
+            Route: 현재 URL과 매칭된 UI를 렌더링 하는 역할을 한다.
+          </li>
+        </ul>
+
+        <p>주소창에 localhost:3000/Router1/'one' ''안을 바꾸면서 화면을 바꾼다.</p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <RouterPractice />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+function RouterPractice() {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="Router1/one" element={<One name="licat" />} />
+      <Route path="Router1/two" element={<Two />} />
+      <Route path="Router1/three" element={<Three />} />
+    </Routes>
+  )
+}
 
 export function Index() {
   return <h1>hello world0</h1>;
--- React/2023.05.12 React/src/6th Class/Router2.jsx
+++ React/2023.05.12 React/src/6th Class/Router2.jsx
@@ -1,14 +1,132 @@
 import React from 'react';
 import { Routes, Route, Link, useLocation, Outlet } from 'react-router-dom';
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
 
 export default function Router2() {
   return (
-    <div>
-      <h1>Router2</h1>
-      <Link to="/">Home</Link>
-      <Link to="/Router2/first">first</Link>
-      <Link to="/Router2/second">second</Link>
-      <Link to="/Router2/third">third</Link>
+    <main style={{margin: "0 300px"}}>
+      <Example1 />
+    </main>
+  );
+}
+
+function Example1() {
+  const codeString = `
+    function RouterPractice() {
+      return (
+        <>
+          <Link to="/">Home</Link>
+          <Link to="/Router2/first">first</Link>
+          <Link to="/Router2/second">second</Link>
+          <Link to="/Router2/third">third</Link>
+
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/first" element={<First name="licat" />} />
+            <Route path="/second" element={<Second />} />
+            <Route path="/third/*" element={<Outlet />}>
+              <Route index element={<HojunIndex />} />
+              <Route path="hojunone" element={<HojunOne />} />
+              <Route path="hojunsecond" element={<Hojunsecond />} />
+            </Route>
+            <Route path="/blog/:id" element={<Blog />} />
+            {/* :/id에 값이 들어오면서 해당되는 페이지를 출력한다. */}
+
+            {/* *: 모든 url을 처리하며 부모 router에는 outlet을 넣어줘야
+            중첩된 url을 처리할 수 있다.
+            Route 안에 Route가 있는데
+            Three 뒤에 아무것도 없으면 hojunindex를
+            hojunone이 있으면 hojunone
+            hojuntwo가 있으면 hojuntwo 를 출력한다 */}
+          </Routes>
+        </>
+      )
+    }
+
+    export function Index() {
+      return <h1>Hello World 0</h1>;
+    }
+
+    export function First({ name }) {
+      return <h1>{name} World 1</h1>;
+    }
+
+    export function Second() {
+      return <h1>Hello World 2</h1>;
+    }
+
+    export function Blog() {
+      const location = useLocation();
+      console.log(location);
+      return <h1>Hello Blog</h1>;
+    }
+
+    export function HojunIndex() {
+      const location = useLocation();
+      console.log(location);
+      return <h1>Hello Hojun Index</h1>;
+    }
+
+    export function HojunOne() {
+      const location = useLocation();
+      console.log(location);
+      return <h1>Hello Hojun 1</h1>;
+    }
+
+    export function Hojunsecond() {
+      const location = useLocation();
+      console.log(location);
+      return <h1>Hello Hojun 2</h1>;
+    }
+
+  `
+  
+  return (
+    <Wrap>
+      <Title>Router</Title>
+        <p>
+          Link는 React Router에서 제공하는 컴포넌트 중 하나로, 클릭하면 애플리케이션 내에서 새로운 경로로 이동하는 링크를 생성하는 컴포넌트이다.
+          <br />
+          Link에는 다양한 속성이 존재한다.
+        </p> 
+
+        <ul>
+        <li>pathName: 이동할 경로를 나타내는 문자열이다.</li>
+        <li>search: 쿼리스트링을 나타내는 문자열이다.</li>
+        <li>hash: URL 해시를 나타내는 문자열이다.</li>
+        <li>state: 이동할 경로와 함께 전달할 상태 정보이다.</li>
+        </ul>
+        
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <RouterPractice />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+
+
+
+function RouterPractice() {
+  return (
+    <>
+      <Link to="/"> Home </Link>
+      <Link to="/Router2/first"> first </Link>
+      <Link to="/Router2/second"> second </Link>
+      <Link to="/Router2/third"> third </Link>
 
       <Routes>
         <Route path="/" element={<Index />} />
@@ -29,8 +147,8 @@ export default function Router2() {
         hojunone이 있으면 hojunone
         hojuntwo가 있으면 hojuntwo 를 출력한다 */}
       </Routes>
-    </div>
-  );
+    </>
+  )
 }
 
 export function Index() {
