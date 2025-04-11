53a99b48bde2fc5aba3b861889af2f6a1b645a2e
5647kr <5647kr@gmail.com>
2025-04-11 16:12:41 +0900

    React Composition Component

--- React/2023.05.12 React/src/4th Class/CompositionComponent1.jsx
+++ React/2023.05.12 React/src/4th Class/CompositionComponent1.jsx
@@ -1,14 +1,85 @@
 import React, { useState } from 'react'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
 
 export default function CompositionComponent1() {
   return (
-    <div>
-      <h1>CompositionComponent1</h1>
-
-      <Screen />
-    </div>
+    <main style={{margin: "0 300px"}}>
+      <Example1 />
+    </main>
   )
 }
+
+function Example1() {
+  const codeString1 = `
+  function Button(props) {
+    const { text, onClick } = props;
+    return (
+      <button onClick={onClick}>{text}</button>
+    );
+  }
+
+  function Modal(props) {
+    const { title, content, onClose } = props;
+    return (
+      <div>
+        <h2>{title}</h2>
+        <p>{content}</p>
+        <Button text="Close" onClick={onClose} />
+      </div>
+    );
+  }
+
+  function Screen() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    function handleOpenModal() {
+      setIsModalOpen(true);
+    }
+
+    function handleCloseModal() {
+      setIsModalOpen(false);
+    }
+
+    return (
+      <div>
+        <h1>Welcome to my app!</h1>
+        <button onClick={handleOpenModal}>Open modal</button>
+        {isModalOpen && (
+          <Modal
+            title="My modal"
+            content="This is my modal."
+            onClose={handleCloseModal}
+          />
+        )}
+      </div>
+    );
+  }
+  `
+
+  return (
+    <Wrap>
+      <Title>합성 컴포넌트</Title>
+        <p>일반적인 컴포넌트를 하나 만들고 그 안에 세부 기능을 가지는 컴포넌트를 합성해서 사용할 수 있다. 컴포넌트를 합성함으로써 컴포넌트의 재사용성을 높일 수 있다.
+
+        </p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString1}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <Screen />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
 
 function Button(props) {
   const { text, onClick } = props;
--- React/2023.05.12 React/src/4th Class/CompositionComponent2.jsx
+++ React/2023.05.12 React/src/4th Class/CompositionComponent2.jsx
@@ -1,17 +1,202 @@
 import React from 'react'
 import styled from "styled-components";
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
 
 export default function CompositionComponent2() {
   return (
-    <div>
-      <h1>CompositionComponent2</h1>
-
-      <CardOne/>
-      <CardTwo/>
-      <CardThree />
-    </div>
+    <main style={{margin: "0 300px"}}>
+      <Example1 />
+      <Example2 />
+      <Example3 />
+    </main>
   )
 }
+
+
+function Example1() {
+  const codeString = `
+    const CardDiv = styled.div\`
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid #c4c4c4;
+    margin-bottom: 20px;
+    width: 200px;
+  \`;
+
+  const CardOne = (props) => {
+    return (
+      <>
+        <CardDiv>
+          <h3>챌린지 설정</h3>
+          <hr />
+          <button>초기화</button>
+          <button>저장하기</button>
+        </CardDiv>
+      </>
+    );
+  };
+  `
+  
+  return (
+    <Wrap>
+      <Title>합성 컴포넌트</Title>
+        <p>일반적인 컴포넌트를 하나 만들고 그 안에 세부 기능을 가지는 컴포넌트를 합성해서 사용할 수 있다. 컴포넌트를 합성함으로써 컴포넌트의 재사용성을 높일 수 있다.
+
+        </p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <CardOne/>
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+function Example2() {
+  const codeString = `
+    const CardDivTwo = styled.div\`
+      padding: 20px;
+      border-radius: 10px;
+      border: 1px solid #c4c4c4;
+      margin-bottom: 20px;
+      width: 400px;
+    \`;
+
+    const CardTwo = (props) => {
+      return (
+        <>
+          <CardDivTwo>
+            <h3>서비스 공유하기</h3>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ut
+              eveniet, laudantium, deleniti autem sequi molestias magni quia,
+              aliquam et praesentium nostrum dolores culpa cupiditate unde
+              doloremque labore beatae accusamus.
+            </p>
+            <div>
+              <button>이미지 저장</button>
+              <button>트위터</button>
+              <button>페이스북</button>
+            </div>
+          </CardDivTwo>
+        </>
+      );
+    };
+  `
+  
+  return (
+    <Wrap>
+      <Title>합성 컴포넌트</Title>
+        <p>일반적인 컴포넌트를 하나 만들고 그 안에 세부 기능을 가지는 컴포넌트를 합성해서 사용할 수 있다. 컴포넌트를 합성함으로써 컴포넌트의 재사용성을 높일 수 있다.
+
+        </p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <CardTwo/>
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+function Example3() {
+  const codeString = `
+    const Card = (props) => {
+      console.log(props)
+      return (
+        <CardDiv className={props.className}>
+          <h3>{props.value}</h3>
+          <hr />
+          <div>{props.children}</div>
+        </CardDiv>
+      );
+    };
+
+    function CardThree() {
+      return (
+        <>
+          <Card className="setting" value="챌린지 설정">
+            <SettingCard />
+          </Card>
+          <Card className="share" value="서비스 공유하기">
+            <ShareCard />
+          </Card>
+        </>
+      );
+    }
+
+    const SettingCard = () => {
+      return (
+        <>
+          <button>초기화</button>
+          <button>저장하기</button>
+        </>
+      );
+    };
+
+
+    const ShareCard = () => {
+      return (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ut
+            eveniet, laudantium, deleniti autem sequi molestias magni quia, aliquam
+            et praesentium nostrum dolores culpa cupiditate unde doloremque labore
+            beatae accusamus.
+          </p>
+          <div>
+            <button>이미지 저장</button>
+            <button>트위터</button>
+            <button>페이스북</button>
+          </div>
+        </>
+      );
+    };
+  `
+  
+  return (
+    <Wrap>
+      <Title>합성 컴포넌트</Title>
+        <p>일반적인 컴포넌트를 하나 만들고 그 안에 세부 기능을 가지는 컴포넌트를 합성해서 사용할 수 있다. 컴포넌트를 합성함으로써 컴포넌트의 재사용성을 높일 수 있다.
+
+        </p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <CardThree/>
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
 
 const CardDiv = styled.div`
   padding: 20px;
