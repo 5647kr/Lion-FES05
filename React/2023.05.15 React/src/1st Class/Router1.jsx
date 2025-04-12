8d0b95702b50a60b89699ddcfa6f4c081c0890e6
5647kr <5647kr@gmail.com>
2025-04-12 15:07:37 +0900

    React Router

--- React/2023.05.15 React/src/1st Class/Router1.jsx
+++ React/2023.05.15 React/src/1st Class/Router1.jsx
@@ -1,33 +1,174 @@
-import { Routes, Route } from 'react-router-dom';
+import { useLocation, useParams, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
+
 
 export default function Router1() {
   return (
-    <div>
-      <h1>Router1</h1>
-
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="Router1/one" element={<One name="licat" />} />
-        <Route path="Router1/two" element={<Two />} />
-        <Route path="Router1/three" element={<Three />} />
-      </Routes>
-    </div>
+    <main style={{margin: "0 300px"}}>
+      <Example1 />
+    </main>
   );
 }
-// 주소창에 localhost:3000'/one' ''안을 바꾸면서 화면을 바꾼다. 
 
-export function Index() {
-  return <h1>hello world0</h1>;
+function Routers() {
+  return (
+    <div>
+      <Link to="/"> home </Link>
+      <Link to="/Router1/one"> one </Link>
+      <Link to="/Router1/two"> two </Link>
+      <Link to="/Router1/three"> three </Link>
+      <Link to="/Router1/blog"> blog </Link>
+      {/* 라우트를 감싸줍니다. */}
+      <Routes>
+        <Route path="/" element={<Index />}/>
+        <Route path="/one" element={<One name='licat' />}/>
+        <Route path="/two" element={<Two />}/>
+        <Route path="/three/*" element={<Outlet />}>
+          <Route index element={<HojunIndex />} />
+          <Route path="hojunone" element={<HojunOne />} />
+          <Route path="hojuntwo" element={<HojunTwo />} />
+        </Route>
+        <Route path="/blog/:id" element={<Blog />}/>
+      </Routes>
+    </div>
+  )
 }
 
-export function One({ name }) {
-  return <h1>{name} world1</h1>;
+export function Index(){
+  return <h1>hello world0</h1>
 }
 
-export function Two() {
-  return <h1>hello world2</h1>;
+export function One({name}){
+  return <h1>{name} world1</h1>
 }
 
-export function Three() {
-  return <h1>hello world3</h1>;
+export function Two(){
+  return <h1>hello world2</h1>
+}
+
+export function Three(){
+  return <h1>hello world3</h1>
+}
+
+export function Blog(){
+  const location = useLocation();
+  const {id} = useParams();
+  console.log(location)
+  return <h1>hello Blog {id}</h1>
+}
+
+export function HojunIndex() {
+  return <h2>hello Hojun index</h2>
+}
+
+export function HojunOne() {
+  return <h2>hello Hojun 1</h2>
+}
+
+export function HojunTwo() {
+  return <h2>hello Hojun 2</h2>
+}
+
+
+
+function Example1() {
+  const codeString = `
+    function Routers() {
+      return (
+        <div>
+          <Link to="/"> home </Link>
+          <Link to="/one"> one </Link>
+          <Link to="/two"> two </Link>
+          <Link to="/three"> three </Link>
+          {/* 라우트를 감싸줍니다. */}
+          <Routes>
+            <Route path="/" element={<Index />}/>
+            <Route path="Router1/one" element={<One name='licat' />}/>
+            <Route path="/two" element={<Two />}/>
+            <Route path="/three/*" element={<Outlet />}>
+              <Route index element={<HojunIndex />} />
+              <Route path="hojunone" element={<HojunOne />} />
+              <Route path="hojuntwo" element={<HojunTwo />} />
+            </Route>
+            <Route path="/blog/:id" element={<Blog />}/>
+          </Routes>
+        </div>
+      )
+    }
+
+    export function Index(){
+      return <h1>hello world0</h1>
+    }
+
+    export function One({name}){
+      return <h1>{name} world1</h1>
+    }
+
+    export function Two(){
+      return <h1>hello world2</h1>
+    }
+
+    export function Three(){
+      return <h1>hello world3</h1>
+    }
+
+    export function Blog(){
+      const location = useLocation();
+      const {id} = useParams();
+      console.log(location)
+      return <h1>hello Blog {id}</h1>
+    }
+
+    export function HojunIndex() {
+      return <h2>hello Hojun index</h2>
+    }
+
+    export function HojunOne() {
+      return <h2>hello Hojun 1</h2>
+    }
+
+    export function HojunTwo() {
+      return <h2>hello Hojun 2</h2>
+    }
+  `
+  
+  return (
+    <Wrap>
+      <Title>Router</Title>
+        <p>
+          라우터는 URL 주소창을 지켜보며, 변화하면 변화에 알맞는 페이지를 사용자에게 안내해주는 기능이다.<br />
+          라우터에는 BrowserRouter, Routes, Route등이 있다.
+        </p>
+        <ul>
+          <li>
+            BrowserRouter: URL과 URL을 연결한다. 마치 SSR과 같이 URL을 사용할 수 있다.
+          </li>
+          <li>
+            Routes: URL이 변경되면 Routes의 모든 자식요소 Route를 살펴보고 가장 알맞는 것을 매칭한다.
+          </li>
+          <li>
+            Route: 현재 URL과 매칭된 UI를 렌더링 하는 역할을 한다.
+          </li>
+          <li>
+            Outlet: 자식의 경로로 들어가면 outlet이 해당 컴포넌트로 바뀐다. 메타몽같은 역할을 한다.
+          </li>
+        </ul>
+
+        <p>주소창에 localhost:3000/Router1/'one' ''안을 바꾸면서 화면을 바꾼다.</p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <Routers />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
 }
--- React/2023.05.15 React/src/2nd Class/Router2.jsx
+++ React/2023.05.15 React/src/2nd Class/Router2.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useParams } from 'react-router-dom';
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
 
 export default function Router2() {
   return (
+    <main style={{margin: "0 300px"}}>
+      <Example1 />
+    </main>
+  );
+}
+
+function Routers() {
+  return (
     <div>
-      <h1>Router2</h1>
       <Link to="/">Home</Link>
       <br />
-      <Link to="/Router2/Cart">Cart</Link>
+      <Link to="/Router2/cart">Cart</Link>
       <br />
-      <Link to="/Router2/Users">Users</Link>
+      <Link to="/Router2/users">Users</Link>
       <br />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -22,7 +30,7 @@ export default function Router2() {
         <Route path="/users/notice" element={<UsersNotice />} />
       </Routes>
     </div>
-  );
+  )
 }
 
 export function HomePage() {
@@ -34,11 +42,13 @@ export function Cart() {
 }
 
 export function Products() {
-  return <h1>Hello products</h1>;
+  const { id } = useParams();
+  return <h1>Hello products {id}</h1>;
 }
 
 export function ProductsNotice() {
-  return <h1>Hello products notice</h1>;
+  const { id } = useParams();
+  return <h1>Hello products {id} notice</h1>;
 }
 
 export function Users() {
@@ -55,4 +65,101 @@ export function UsersQuestion() {
 
 export function UsersNotice() {
   return <h1>UsersNotice Page</h1>;
+}
+
+
+
+function Example1() {
+  const codeString = `
+    function Routers() {
+      return (
+        <div>
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/Router2/Cart">Cart</Link>
+          <br />
+          <Link to="/Router2/Users">Users</Link>
+          <br />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products/:id" element={<Products />} />
+            <Route path="/products/:id/notice" element={<ProductsNotice />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/coupon" element={<UsersCoupon />} />
+            <Route path="/users/question" element={<UsersQuestion />} />
+            <Route path="/users/notice" element={<UsersNotice />} />
+          </Routes>
+        </div>
+      )
+    }
+    
+    export function HomePage() {
+      return <h1>홈페이지</h1>;
+    }
+    
+    export function Cart() {
+      return <h1>Cart Page</h1>;
+    }
+    
+    export function Products() {
+      const { id } = useParams();
+      return <h1>Hello products {id}</h1>;
+    }
+    
+    export function ProductsNotice() {
+      const { id } = useParams();
+      return <h1>Hello products {id} notice</h1>;
+    }
+    
+    export function Users() {
+      return <h1>Users Page</h1>;
+    }
+    
+    export function UsersCoupon() {
+      return <h1>UsersCoupon Page</h1>;
+    }
+    
+    export function UsersQuestion() {
+      return <h1>UsersQuestion Page</h1>;
+    }
+    
+    export function UsersNotice() {
+      return <h1>UsersNotice Page</h1>;
+    }
+  `
+  
+  return (
+    <Wrap>
+      <Title>Router 실습</Title>
+        <p>
+        Quiz - Route 사용하여 페이지 경로 만들기
+        <br />
+        - **Home Page :** /<br />
+        - **Product Detail Page** : /products/:id<br />
+            - ex) /products/1 , /products/2, /products/3, /products/4<br />
+        - **Product Detail Notice Page :** /products/:id/notice<br />
+            - ex) /products/1/notice , /products/2/notice…<br />
+        - **Cart Page :** /cart<br />
+        - **Coupon Page :** /users/coupon<br />
+        - **Question Page :** /users/question<br />
+        - **Notice Page :** /users/notice<br />
+        - **User Page :** /users<br />
+        </p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <Routers />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
 }
