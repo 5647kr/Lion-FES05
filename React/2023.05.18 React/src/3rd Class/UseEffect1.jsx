9f002500da5188d5d1a7572b2e3b61e39777e88c
5647kr <5647kr@gmail.com>
2025-04-14 11:10:03 +0900

    React UseEffect

---
+++ React/2023.05.18 React/src/3rd Class/UseEffect1.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
+
+export default function UseEffect1() {
+  return (
+    <main style={{margin: "0 100px"}}>
+      <Example1/>
+    </main>
+  )
+}
+
+  function EffectVsLayoutEffect() {
+    const [num, setNum] = useState(0)
+
+    useEffect(() => {
+      setNum(10)
+    }, [num])
+
+    return (
+      <>
+        <div>{num}</div>
+        <button onClick={() => setNum(prevNum => prevNum + 1)}>클릭</button>
+      </>
+    )
+  }
+
+
+
+  function Example1() {
+    const codeString = `
+    `
+    
+    return (
+      <Wrap>
+        <Title>useEffect VS useLayoutEffect</Title>
+          <p>
+            useLayoutEffect는 화면을 그리기 이전에 실행, useEffect는 화면을 그린 이후에 실행
+          </p>
+
+        <ContentWrap>
+          <CodeWrap>
+            <strong>코드</strong>
+            <pre>
+              <code>
+                {codeString}
+              </code>
+            </pre>
+          </CodeWrap>
+          <ResultWrap>
+            <strong>실행 결과</strong>
+            <EffectVsLayoutEffect />
+          </ResultWrap>
+        </ContentWrap>
+      </Wrap>
+    )
+  }
