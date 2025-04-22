7ae3cac9e5a914bbf095665bec8dfc95997b81c6
5647kr <5647kr@gmail.com>
2025-04-22 12:00:00 +0900

    React Router

--- React/2023.05.15 React/src/2nd Class/Router2.jsx
+++ React/2023.05.15 React/src/2nd Class/Router2.jsx
@@ -71,62 +71,62 @@ export function UsersNotice() {
 
 function Example1() {
   const codeString = `
-    function Routers() {
-      return (
-        <div>
-          <Link to="/">Home</Link>
-          <br />
-          <Link to="/Router2/Cart">Cart</Link>
-          <br />
-          <Link to="/Router2/Users">Users</Link>
-          <br />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products/:id" element={<Products />} />
-            <Route path="/products/:id/notice" element={<ProductsNotice />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/users/coupon" element={<UsersCoupon />} />
-            <Route path="/users/question" element={<UsersQuestion />} />
-            <Route path="/users/notice" element={<UsersNotice />} />
-          </Routes>
-        </div>
-      )
-    }
-    
-    export function HomePage() {
-      return <h1>홈페이지</h1>;
-    }
-    
-    export function Cart() {
-      return <h1>Cart Page</h1>;
-    }
-    
-    export function Products() {
-      const { id } = useParams();
-      return <h1>Hello products {id}</h1>;
-    }
-    
-    export function ProductsNotice() {
-      const { id } = useParams();
-      return <h1>Hello products {id} notice</h1>;
-    }
-    
-    export function Users() {
-      return <h1>Users Page</h1>;
-    }
-    
-    export function UsersCoupon() {
-      return <h1>UsersCoupon Page</h1>;
-    }
-    
-    export function UsersQuestion() {
-      return <h1>UsersQuestion Page</h1>;
-    }
-    
-    export function UsersNotice() {
-      return <h1>UsersNotice Page</h1>;
-    }
+function Routers() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/Router2/Cart">Cart</Link>
+      <br />
+      <Link to="/Router2/Users">Users</Link>
+      <br />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products/:id" element={<Products />} />
+        <Route path="/products/:id/notice" element={<ProductsNotice />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/coupon" element={<UsersCoupon />} />
+        <Route path="/users/question" element={<UsersQuestion />} />
+        <Route path="/users/notice" element={<UsersNotice />} />
+      </Routes>
+    </div>
+  )
+}
+
+export function HomePage() {
+  return <h1>홈페이지</h1>;
+}
+
+export function Cart() {
+  return <h1>Cart Page</h1>;
+}
+
+export function Products() {
+  const { id } = useParams();
+  return <h1>Hello products {id}</h1>;
+}
+
+export function ProductsNotice() {
+  const { id } = useParams();
+  return <h1>Hello products {id} notice</h1>;
+}
+
+export function Users() {
+  return <h1>Users Page</h1>;
+}
+
+export function UsersCoupon() {
+  return <h1>UsersCoupon Page</h1>;
+}
+
+export function UsersQuestion() {
+  return <h1>UsersQuestion Page</h1>;
+}
+
+export function UsersNotice() {
+  return <h1>UsersNotice Page</h1>;
+}
   `
   
   return (
