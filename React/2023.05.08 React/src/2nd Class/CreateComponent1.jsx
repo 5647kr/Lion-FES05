9321a13fe55988eb04369acb2c569e1c4a4ca334
5647kr <5647kr@gmail.com>
2025-04-24 15:34:28 +0900

    React Component

--- React/2023.05.08 React/src/2nd Class/CreateComponent1.jsx
+++ React/2023.05.08 React/src/2nd Class/CreateComponent1.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
+
+
 
 export default function CreateComponent1() {
   return (
-    <div>
-      <h1>CreateComponent</h1>
-      <Refactor />
-      <User name="개리" />
-      <Time />
-      {/* 컴포넌트 이름은 대문자로 */}
-    </div>
+    <main style={{margin: "0 100px"}}>
+      <Example1/>
+      <Example2/>
+    </main>
   )
 }
 
-export function Refactor() {
+function ComponentPractice1() {
   const name = '라이캣!';
   const someStyle = {backgroundColor:"black", color:"white"};
   const today = new Date();
@@ -39,7 +39,70 @@ export function Refactor() {
   );
 }
 
-export function User(props) {
+
+function Example1() {
+  const codeString = `
+function ComponentPractice1() {
+  const name = '라이캣!';
+  const someStyle = {backgroundColor:"black", color:"white"};
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth()+1;
+  const date = today.getDate();
+  const hour = today.getHours();
+  const min = today.getMinutes();
+  const sec = today.getSeconds();
+
+  return (
+    <div>
+      <div>
+        <h1 style={someStyle}>안녕, {name} 1호</h1>
+        <h1>안녕, 라이캣 2호!</h1>
+        <div className="newClass"/>
+      </div>
+      <div style={{backgroundColor:"black", color:"white"}}>
+        <h1 style={{color:'red'}}>년 : {year}</h1>
+        <h1>월/일 : {month}/{date}</h1>
+        <h1>시간 : {hour}시 {min}분 {sec}초</h1>
+      </div>
+    </div>
+  );
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Component 만들기</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <ComponentPractice1 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+function ComponentPractice2() {
+  return (
+    <div>
+      <User name="개리" />
+      <Time />
+    </div>
+  )
+}
+
+function User(props) {
   const name2 = '라이캣!';
   const someStyle2 = {backgroundColor:"black", color:"white"};
   
@@ -52,7 +115,7 @@ export function User(props) {
   )
 }
 
-export function Time() {
+function Time() {
   const today1 = new Date();
   const year1 = today1.getFullYear();
   const month1 = today1.getMonth()+1;
@@ -67,5 +130,69 @@ export function Time() {
       <h1>월/일 : {month1}/{date1}</h1>
       <h1>시간 : {hour1}시 {min1}분 {sec1}초</h1>
   </div>
+  )
+}
+
+function Example2() {
+  const codeString = `
+function ComponentPractice2() {
+  return (
+    <div>
+      <User name="개리" />
+      <Time />
+    </div>
+  )
+}
+
+function User(props) {
+  const name2 = '라이캣!';
+  const someStyle2 = {backgroundColor:"black", color:"white"};
+  
+  return (
+    <div>
+      <h1 style={someStyle2}>안녕, {name2} 1호</h1>
+      <h1>안녕, {props.name} 2호!</h1>
+      <div className="newClass"/>
+    </div>
+  )
+}
+
+function Time() {
+  const today1 = new Date();
+  const year1 = today1.getFullYear();
+  const month1 = today1.getMonth()+1;
+  const date1 = today1.getDate();
+  const hour1 = today1.getHours();
+  const min1 = today1.getMinutes();
+  const sec1 = today1.getSeconds();
+
+  return (
+    <div style={{backgroundColor:"black", color:"white"}}>
+      <h1 style={{color:'red'}}>년 : {year1}</h1>
+      <h1>월/일 : {month1}/{date1}</h1>
+      <h1>시간 : {hour1}시 {min1}분 {sec1}초</h1>
+  </div>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Component 나눠보기</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <ComponentPractice2 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
   )
 }
--- React/2023.05.08 React/src/3rd Class/CreateComponent2.jsx
+++ React/2023.05.08 React/src/3rd Class/CreateComponent2.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
+
+
 
 export default function CreateComponent2() {
   return (
-    <div>
-      <h1>CreateComponent2</h1>
-
-      <HelloProps 
-				name="jaehyun" 
-				age={25} 
-				someFunc={() => 'awesome!!!'} 
-				someJSX={<img src="https://picsum.photos/id/237/200/300" />} 
-				someArr={[1, 2, 3]} 
-				someObj={{ one: 1 }} />
-
-      <Resume 
-        hello="안녕하세요" 
-        name="개리" 
-        hobby="게임" 
-        food="고기" 
-        color="blue"/>
-
-        <ColorText color="red"/>
-        <ColorText color="green"/>
-        <ColorText color="blue"/>
-    </div>
+    <main style={{margin: "0 100px"}}>
+      <Example1/>
+      <Example2/>
+      <Example3/>
+    </main>
   )
 }
 
-export function HelloProps(props) {
+
+
+
+
+function ComponentPractice1() {
+  return (
+    <>
+      <HelloProps 
+      name="jaehyun" 
+      age={25} 
+      someFunc={() => 'awesome!!!'} 
+      someJSX={<img src="https://picsum.photos/id/237/200/300" />} 
+      someArr={[1, 2, 3]} 
+      someObj={{ one: 1 }} />
+    </>
+  )
+}
+
+function HelloProps(props) {
   return (
     <div>
       <p>my name is {props.name} and age is {props.age}</p>
@@ -39,7 +43,77 @@ export function HelloProps(props) {
   )
 }
 
-export function Resume(props) {
+function Example1() {
+  const codeString = `
+function ComponentPractice1() {
+  return (
+    <>
+      <HelloProps 
+      name="jaehyun" 
+      age={25} 
+      someFunc={() => 'awesome!!!'} 
+      someJSX={<img src="https://picsum.photos/id/237/200/300" />} 
+      someArr={[1, 2, 3]} 
+      someObj={{ one: 1 }} />
+    </>
+  )
+}
+
+function HelloProps(props) {
+  return (
+    <div>
+      <p>my name is {props.name} and age is {props.age}</p>
+      <strong>you are {props.someFunc()}</strong>
+      <p>this is someArr {[...props.someArr]}</p>
+      <p>this is someObj {props.someObj.one}</p>
+      {props.someJSX}
+    </div>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Component 만들기</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <ComponentPractice1 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+
+
+
+
+
+function ComponentPractice2() {
+  return (
+    <>
+      <Resume 
+      hello="안녕하세요" 
+      name="개리" 
+      hobby="게임" 
+      food="고기" 
+      color="blue"/>
+    </>
+  )
+}
+
+function Resume(props) {
   return (
     <>
       <h1>{props.name} 자기소개서</h1>
@@ -51,10 +125,121 @@ export function Resume(props) {
   )
 }
 
-export function ColorText(props) {
+
+function Example2() {
+  const codeString = `
+function ComponentPractice2() {
+  return (
+    <>
+      <Resume 
+      hello="안녕하세요" 
+      name="개리" 
+      hobby="게임" 
+      food="고기" 
+      color="blue"/>
+    </>
+  )
+}
+
+function Resume(props) {
+  return (
+    <>
+      <h1>{props.name} 자기소개서</h1>
+      <h2>{props.hello}</h2>
+      <p>취미: {props.hobby}</p>
+      <p>좋아하는 음식: {props.food}</p>
+      <p>좋아하는 색: <span style={{color: "blue"}}>{props.color}</span></p>
+    </>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Component 만들기</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <ComponentPractice2 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+function ComponentPractice3() {
+  return (
+    <>
+      <ColorText color="red"/>
+      <ColorText color="green"/>
+      <ColorText color="blue"/>
+    </>
+  )
+}
+
+function ColorText(props) {
   return(
     <>
       <h1 style={{color:props.color}}>색이 바뀌어요!</h1>
     </>
+  )
+}
+
+function Example3() {
+  const codeString = `
+function ComponentPractice3() {
+  return (
+    <>
+      <ColorText color="red"/>
+      <ColorText color="green"/>
+      <ColorText color="blue"/>
+    </>
+  )
+}
+
+function ColorText(props) {
+  return(
+    <>
+      <h1 style={{color:props.color}}>색이 바뀌어요!</h1>
+    </>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Component 만들기</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <ComponentPractice3 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
   )
 }
