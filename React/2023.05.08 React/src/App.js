55a195d7c9bb09fa5a50ab7ce27f16d2fe7b1f66
5647kr <5647kr@gmail.com>
2025-04-24 15:33:47 +0900

    React 링크 설정

--- React/2023.05.08 React/src/App.js
+++ React/2023.05.08 React/src/App.js
@@ -5,8 +5,9 @@ import Fragments1 from './1st Class/Fragments1';
 import Fragments2 from './2nd Class/Fragments2';
 import CreateComponent1 from './2nd Class/CreateComponent1';
 import CreateComponent2 from './3rd Class/CreateComponent2';
-import BasicHook1 from './4th Class/BasicHook1';
-import BasicHook2 from './5th Class/BasicHook2';
+
+import UseStateHook1 from './4th Class/UseStateHook1';
+import UseStateHook2 from './5th Class/UseStateHook2';
 import Mood from './6th Class/Mood';
 import MoodApp from './7th Class/MoodApp';
 
@@ -21,8 +22,8 @@ function App() {
         <Route path='/Fragments2' element={<Fragments2 />} />
         <Route path='/CreateComponent1' element={<CreateComponent1 />} />
         <Route path='/CreateComponent2' element={<CreateComponent2 />} />
-        <Route path='/BasicHook1' element={<BasicHook1 />} />
-        <Route path='/BasicHook2' element={<BasicHook2/>} />
+        <Route path='/UseStateHook1' element={<UseStateHook1 />} />
+        <Route path='/UseStateHook2' element={<UseStateHook2/>} />
         <Route path='/Mood' element={<Mood />} />
         <Route path='/MoodApp' element={<MoodApp />} />
       </Routes>
--- React/2023.05.08 React/src/Main.jsx
+++ React/2023.05.08 React/src/Main.jsx
@@ -11,8 +11,8 @@ export default function Main() {
       <button onClick={() => navigate('/Fragments2')}>2-1교시</button>
       <button onClick={() => navigate('/CreateComponent1')}>2-2교시</button>
       <button onClick={() => navigate('/CreateComponent2')}>3교시</button>
-      <button onClick={() => navigate('/BasicHook1')}>4교시</button>
-      <button onClick={() => navigate('/BasicHook2')}>5교시</button>
+      <button onClick={() => navigate('/UseStateHook1')}>4교시</button>
+      <button onClick={() => navigate('/UseStateHook2')}>5교시</button>
       <button onClick={() => navigate('/Mood')}>6교시</button>
       <button onClick={() => navigate('/MoodApp')}>7교시</button>
     </div>
