58b25c3838be46f440ec21305b4fd2e552c1110e
5647kr <5647kr@gmail.com>
2025-04-24 15:34:17 +0900

    React Fragment

--- React/2023.05.08 React/src/1st Class/Fragments1.jsx
+++ React/2023.05.08 React/src/1st Class/Fragments1.jsx
@@ -1,48 +1,84 @@
 import React, { Fragment } from 'react'
 import './Fragments1.css'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
+
+
 
 export default function Fragments1() {
-  // Fragments를 사용하면 div와 같은 별도의 노드추가없이
-  // 여러 개의 자식을 감쌀 수 있다.
   return (
-    <div>
-      <h1>Fragments</h1>
-      <p>Fragments를 사용하면 div와 같은 별도의 노드 추가 없이 여러개의 자식을 감쌀 수 있다.</p>
-      <p>Fragment사용시, React.Fragment 또는 Fragment 방법으로 작성할 수 있다.</p>
-      <p>단, 스타일 속성을 적용해도 출력되지 않으며, key값을 부여하는데 사용된다.</p>
-      <ListItem />
-      <Glossary />
-      <MyComponent />
-    </div>
+    <main style={{margin: "0 100px"}}>
+      <Example1/>
+    </main>
   )
 }
 
-export function ListItem() {
+function FragmentsPractice() {
   return (
-    // React.Fragment 또는 import후 Fragment 또는 공백
-    <Fragment>
-      <dt>사과</dt>
-      <dd>사과는 가을이 제철이죠!</dd>
-    </Fragment>
-  );
+    <>
+      <React.Fragment className="my-fragment">
+        <h2>안녕 라이켓!</h2>
+        <p>안녕 루디?</p>
+      </React.Fragment>
+
+      <Fragment>
+        <h2>안녕 라이켓!</h2>
+        <p>안녕 루디?</p>
+      </Fragment>
+
+      <>
+        <h2 className='my-fragment'>안녕 라이켓!</h2>
+        <p className='my-fragment'>안녕 루디?</p>
+      </>
+    </>
+  )
 }
 
-export function Glossary() {
-  return (
-    <dl>
-      <ListItem />
-      <ListItem />
-    </dl>
-  );
-}
 
-export function MyComponent() {
+  function Example1() {
+    const codeString = `
+function FragmentsPractice() {
   return (
-    // Fragment에 스타일 속성을 적용해도 출력되지 않아 무의미하다.
-    // Fragment를 단축하면 속성을 적용하지 못한다.
-    <React.Fragment> 
-      <h1 className="my-fragment">리엑트프레그먼트</h1>
-      <p className="my-fragment">테스트입니다!</p>
-    </React.Fragment>
-  );
+    <>
+      <React.Fragment className="my-fragment">
+        <h2>안녕 라이켓!</h2>
+        <p>안녕 루디?</p>
+      </React.Fragment>
+
+      <Fragment>
+        <h2>안녕 라이켓!</h2>
+        <p>안녕 루디?</p>
+      </Fragment>
+
+      <>
+        <h2 className='my-fragment'>안녕 라이켓!</h2>
+        <p className='my-fragment'>안녕 루디?</p>
+      </>
+    </>
+  )
 }
+    `
+    
+    return (
+      <Wrap>
+        <Title>Fragments</Title>
+        <p>Fragments를 사용하면 div와 같은 별도의 노드 추가 없이 여러개의 자식을 감쌀 수 있다.</p>
+      <p>Fragment사용시, React.Fragment 또는 Fragment 방법으로 작성할 수 있다.</p>
+      <p>단, 스타일 속성을 적용해도 출력되지 않으며, key값을 부여하는데 사용된다.</p>
+
+        <ContentWrap>
+          <CodeWrap>
+            <strong>코드</strong>
+            <pre>
+              <code>
+                {codeString}
+              </code>
+            </pre>
+          </CodeWrap>
+          <ResultWrap>
+            <strong>실행 결과</strong>
+            <FragmentsPractice />
+          </ResultWrap>
+        </ContentWrap>
+      </Wrap>
+    )
+  }
--- React/2023.05.08 React/src/2nd Class/Fragments2.jsx
+++ React/2023.05.08 React/src/2nd Class/Fragments2.jsx
@@ -1,19 +1,16 @@
 import React, { Fragment } from 'react'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
 
-export default function Fragments2() {
+
+
+export default function Fragments1() {
   return (
-    <div>
-      <h1>Fragments2</h1>
-        <List />
-        <List1 />
-      <dl>
-        <h2>내풀이</h2>
-        <List2 />
-        <List3 />
-      </dl>
-        <h2>선생님 풀이</h2>
-        <List3T />
-    </div>
+    <main style={{margin: "0 100px"}}>
+      <Example1/>
+      <Example1T/>
+      <Example3/>
+      <Example2T/>
+    </main>
   )
 }
 
@@ -24,88 +21,321 @@ let list = [
   { no: 4, area: "제주도", visited: false },
 ];
 
-export function List() {
+function FragmentsPractice1() {
   return (
-    list.map((item) => {
-      return(
-      <Fragment key={item.no}>
-        <h1>{ item.area }</h1>
-        <p>{ item.visited ? "방문함" : "방문 안함" }</p>
-      </Fragment>)
-    })
-  );
-}
-
-function List1() {
-  let list = [
-    { no: 1, area: "대전", visited: false },
-    { no: 2, area: "부산", visited: true },
-    { no: 3, area: "목포", visited: false },
-    { no: 4, area: "제주도", visited: false },
-  ];
-
-  return (
-    <ul className='list-area'>
+    <ul>
       {list.map((item) => {
         return (
-          <li key={item.no} className={item.visited ? "area-item active" : "area-item"}>{item.area}</li>
+          <li key={item.no}>
+            <p>{item.area}</p>
+          </li>
         )
       })}
     </ul>
   )
 }
 
-// 위 두 코드의 경우 list.map은 동일하나 아래코드는 중괄호로 감싸고 있으나 윗 코드는 중괄호로 감싸고 있지 않다. 
+function FragmentsPractice2() {
+  return (
+    list.map((item) => {
+      return(
+      <Fragment key={item.no}>
+        <p>{ item.area }</p>
+      </Fragment>)
+    })
+  );
+}
 
-// 그 이유는 html태그 안에 js코드를 처리하기 위해 중괄호를 사용하고 그렇지 않은 경우엔 중괄호를 사용하지 않는다.
 
-const items = [
-  { id: 1, name: 'Apple', desc: '빨간건 사과' },
-  { id: 2, name: 'Banana', desc: '바나나는 길어' },
-  { id: 3, name: 'Cherry', desc: '체리는 비싸' }
+function Example1() {
+  const codeString = `
+let list = [
+  { no: 1, area: "대전", visited: false },
+  { no: 2, area: "부산", visited: true },
+  { no: 3, area: "목포", visited: false },
+  { no: 4, area: "제주도", visited: false },
 ];
 
-export function List2() {
+function FragmentsPractice1() {
   return (
-    items.map((item) => {
-      return (
-        <React.Fragment key={item.id}>
-          <dt>{item.name}</dt>
-          <dd>{item.desc}</dd>
-        </React.Fragment>
-      )
-    })
+    <ul>
+      {list.map((item) => {
+        return (
+          <li key={item.no}>
+            <p>{item.area}</p>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
-export function List3() {
+function FragmentsPractice2() {
+  return (
+    list.map((item) => {
+      return(
+      <Fragment key={item.no}>
+        <p>{ item.area }</p>
+      </Fragment>)
+    })
+  );
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Fragments 실습</Title>
+      <p>위 두 코드의 경우 list.map은 동일하나 아래코드는 중괄호로 감싸고 있으나 윗 코드는 중괄호로 감싸고 있지 않다.</p>
+      <p>그 이유는 html태그 안에 js코드를 처리하기 위해 중괄호를 사용하고 그렇지 않은 경우엔 중괄호를 사용하지 않는다.</p>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <FragmentsPractice1 />
+          <FragmentsPractice2 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+function FragmentsPractice1T() {
+  return (
+    list.map((item) => {
+      return(
+      <Fragment key={item.no}>
+        <p>{ item.area }</p>
+        <p>{ item.visited ? "방문함" : "방문 안함" }</p>
+      </Fragment>)
+    })
+  );
+}
+
+
+function Example1T() {
+  const codeString = `
+let list = [
+  { no: 1, area: "대전", visited: false },
+  { no: 2, area: "부산", visited: true },
+  { no: 3, area: "목포", visited: false },
+  { no: 4, area: "제주도", visited: false },
+];
+
+function FragmentsPractice3T() {
+  return (
+    list.map((item) => {
+      return(
+      <Fragment key={item.no}>
+        <p>{ item.area }</p>
+        <p>{ item.visited ? "방문함" : "방문 안함" }</p>
+      </Fragment>)
+    })
+  );
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Fragments 실습T</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <FragmentsPractice1T />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+let items = [
+  { id: 1, name: "Apple", desc: "사과는 빨갛다." },
+  { id: 2, name: "Banana", desc: "바나나는 길다." },
+  { id: 3, name: "Cherry", desc: "체리는 비싸다." },
+];
+
+function FragmentsPractice3() {
   return (
     <dl>
       {items.map((item) => {
         return (
-          <React.Fragment key={item.id}>
+          <Fragment key={item.id}>
             <dt>{item.name}</dt>
-            <dd>{item.desc}</dd>
-          </React.Fragment>
+            <dt>{item.desc}</dt>
+          </Fragment>
         )
       })}
     </dl>
   )
 }
 
-export function List3T() {
-  const itemList = items.map(({id, name, desc})=> {
-    return(
+
+function Example3() {
+  const codeString = `
+let items = [
+  { id: 1, name: "Apple", desc: "사과는 빨갛다." },
+  { id: 2, name: "Banana", desc: "바나나는 길다." },
+  { id: 3, name: "Cherry", desc: "체리는 비싸다." },
+];
+
+function FragmentsPractice3() {
+  return (
+    <dl>
+      {items.map((item) => {
+        return (
+          <Fragment key={item.id}>
+            <dt>{item.name}</dt>
+            <dt>{item.desc}</dt>
+          </Fragment>
+        )
+      })}
+    </dl>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Fragments 실습2</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <FragmentsPractice3 />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+function FragmentsPractice2T() {
+  const itemList = items.map(({id, name, desc}) => {
+    return (
       <React.Fragment key={id}>
-        <dt>{name}</dt>
+        <dd>{name}</dd>
         <dd>{desc}</dd>
       </React.Fragment>
     )
   })
-
   return (
     <dl>
       {itemList}
     </dl>
   )
 }
+
+
+function Example2T() {
+  const codeString = `
+function FragmentsPractice2T() {
+  const itemList = items.map(({id, name, desc}) => {
+    return (
+      <React.Fragment key={id}>
+        <dd>{name}</dd>
+        <dd>{desc}</dd>
+      </React.Fragment>
+    )
+  })
+  return (
+    <dl>
+      {itemList}
+    </dl>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>Fragments 실습2T</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <FragmentsPractice2T />
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
