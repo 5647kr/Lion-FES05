cccf0df2847828c5c09ef6ae64368e2a260b1b00
5647kr <5647kr@gmail.com>
2025-04-25 09:23:27 +0900

    React ComponentList

--- React/2023.05.09 React/src/6th Class/ComponentList.jsx
+++ React/2023.05.09 React/src/6th Class/ComponentList.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonStyle'
+
 
 export default function ComponentList() {
   return (
-    <div>
-      <h1>ComponentList</h1>
-      <Hello />
-    </div>
+    <main style={{margin: "0 100px"}}>
+      <Example1/>
+    </main>
   )
 }
 
-function Hello(props) {
+function ComponentListPractice1(props) {
 	const name = props.name;
   const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //1~10호를 만들 것입니다.
 	
@@ -17,7 +18,45 @@ function Hello(props) {
 
   return(
     <div>
-      {num.map((i)=><h1>안녕, {name} {i}호</h1>)}
+      {num.map((i)=><h1 key={i}>안녕, {name} {i}호</h1>)}
     </div>
+  )
+}
+
+
+function Example1() {
+  const codeString = `
+function ComponentListPractice1(props) {
+	const name = props.name;
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //1~10호를 만들 것입니다.
+	
+  // list map을 이용하여 h1 태그들의 리스트를 만듭니다.
+
+  return(
+    <div>
+      {num.map((i)=><h1 key={i}>안녕, {name} {i}호</h1>)}
+    </div>
+  )
+}
+  `
+  
+  return (
+    <Wrap>
+      <Title>ComponentList</Title>
+      <ContentWrap>
+        <CodeWrap>
+          <strong>코드</strong>
+          <pre>
+            <code>
+              {codeString}
+            </code>
+          </pre>
+        </CodeWrap>
+        <ResultWrap>
+          <strong>실행 결과</strong>
+          <ComponentListPractice1 name="라이켓"/>
+        </ResultWrap>
+      </ContentWrap>
+    </Wrap>
   )
 }
