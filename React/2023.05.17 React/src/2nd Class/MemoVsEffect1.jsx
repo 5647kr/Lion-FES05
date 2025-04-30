f548137ca2cf973b8bcd4cf3bd1ceec3df05b468
5647kr <5647kr@gmail.com>
2025-04-30 14:27:52 +0900

    React UseMemoVsUseEffect

--- React/2023.05.17 React/src/2nd Class/MemoVsEffect1.jsx
+++ React/2023.05.17 React/src/2nd Class/MemoVsEffect1.jsx
@@ -91,74 +91,73 @@ export default function MemoVsEffect1() {
 
   function Example1() {
     const codeString = `
-      function UseMemoPractice1() {
-        const inputName = useRef(null);
-        const inputId = useRef(null)
-        const [userInfo, setUserInfo] = useState([]);
-        const [name, setName] = useState('');
-        const [id, setId] = useState('');
+function UseMemoPractice1() {
+  const inputName = useRef(null);
+  const inputId = useRef(null)
+  const [userInfo, setUserInfo] = useState([]);
+  const [name, setName] = useState('');
+  const [id, setId] = useState('');
 
 
-        function handleSubmit(e) {
-          e.preventDefault();
-          const newInfo = [...userInfo, { name, id }];
-          setUserInfo(newInfo);
-          console.log('렌더링 - 3');
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newInfo = [...userInfo, { name, id }];
+    setUserInfo(newInfo);
+    console.log('렌더링 - 3');
+  }
+  const handleInputName = (event) => {
+    console.log('렌더링 - 1');
+    setName(event.target.value);
+  }
+
+  const handleInputId = (event) => {
+    console.log('렌더링 - 2');
+    setId(event.target.value);
+  }
+
+  // 모든 렌더링에 함께 렌더링되는 이슈가 있습니다.
+  function getNum(li) {
+    console.log('렌더링!');
+    return li.length;
+  }
+
+  const num = useMemo(() => {
+    return getNum(userInfo)
+  }, [userInfo])
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='이름을 입력하세요'
+          onChange={handleInputName}
+          value={name}
+          ref={inputName}
+        />
+        <input
+          type='text'
+          placeholder='아이디를 입력하세요'
+          onChange={handleInputId}
+          value={id}
+          ref={inputId}
+        />
+        <button type='submit'>회원 명부 작성</button>
+      </form>
+      <ul>
+        {
+          userInfo.map((value, index) => (
+            <li key={index}>
+              <h3>이름 : {value.name}</h3>
+              <strong>아이디 : {value.id}</strong>
+            </li>
+          ))
         }
-        const handleInputName = (event) => {
-          console.log('렌더링 - 1');
-          setName(event.target.value);
-        }
-
-        const handleInputId = (event) => {
-          console.log('렌더링 - 2');
-          setId(event.target.value);
-        }
-
-        // 모든 렌더링에 함께 렌더링되는 이슈가 있습니다.
-        function getNum(li) {
-          console.log('렌더링!');
-          return li.length;
-        }
-
-        const num = useMemo(() => {
-          return getNum(userInfo)
-        }, [userInfo])
-
-        
-        return (
-          <>
-            <form onSubmit={handleSubmit}>
-              <input
-                type='text'
-                placeholder='이름을 입력하세요'
-                onChange={handleInputName}
-                value={name}
-                ref={inputName}
-              />
-              <input
-                type='text'
-                placeholder='아이디를 입력하세요'
-                onChange={handleInputId}
-                value={id}
-                ref={inputId}
-              />
-              <button type='submit'>회원 명부 작성</button>
-            </form>
-            <ul>
-              {
-                userInfo.map((value, index) => (
-                  <li key={index}>
-                    <h3>이름 : {value.name}</h3>
-                    <strong>아이디 : {value.id}</strong>
-                  </li>
-                ))
-              }
-            </ul>
-            <span>현재 회원 수 : {num}</span>
-          </>
-        )
-      }
+      </ul>
+      <span>현재 회원 수 : {num}</span>
+    </>
+  )
+}
     `
     
     return (
