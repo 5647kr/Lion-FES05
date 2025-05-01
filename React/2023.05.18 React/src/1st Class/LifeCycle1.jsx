e8de838f7f2db26a079ddfc97d9a61ae570546b1
5647kr <5647kr@gmail.com>
2025-05-01 11:50:24 +0900

    React LifeCycle

--- React/2023.05.18 React/src/1st Class/LifeCycle1.jsx
+++ React/2023.05.18 React/src/1st Class/LifeCycle1.jsx
@@ -3,7 +3,7 @@ import { Wrap, Title, ContentWrap, CodeWrap, ResultWrap } from '../style/CommonS
 
 export default function LifeCycle1() {
   return (
-    <main style={{margin: "0 100px"}}>
+    <main style={{margin: "0 300px"}}>
       <Example1/>
     </main>
   )
@@ -78,70 +78,70 @@ export default function LifeCycle1() {
 
   function Example1() {
     const codeString = `
-      class ClassComp extends Component {
-        constructor(props) {
-          super(props);
-          this.state = { number: 0 };
-        }
-      
-        componentWillMount() {
-          //컴포넌트가 생성될 때 실행됩니다. (렌더링 되기 이전)
-          console.log("componentWillMount");
-        }
-      
-        componentDidMount() {
-        //마운트가 완료되고 나서 실행됩니다. (렌더링 이후) 
-          console.log("componentDidMount");
-        }
-      
-        shouldComponentUpdate(nextProps, nextState) {
-          //상태 업데이트가 되고 컴포넌트가 업데이트 되기 전(렌더되기 전) 실행됩니다.
-          //초기 렌더링에서 발생하지 않습니다.
-          console.log("shouldComponentUpdate");
-          return true;
-        }
-      
-        componentWillUpdate(nextProps, nextState) {
-          //UNSAFE_componentWillUpdate()로 변경됨, 17버전까지는 사용 가능합니다.
-          //초기 렌더링에서 발생하지 않고, 그 이후 업데이트가 되고 나서 발생합니다.
-          console.log("componetWillUpdate");
-        }
-      
-        componentDidUpdate(nextProps, nextState) {
-          //업데이트가 되고 나서 실행됩니다.
-          console.log("componentDidUpdate");
-        }
-      
-        componentWillUnmount() {
-        //컴포넌트가 제거되기 바로 직전에 실행됩니다.
-          console.log("componentWillUnmount");
-        }
-        
-      
-        handleClick = () => {
-          this.setState((state) => {
-            return {
-              number: state.number + 1,
-            };
-          });
-        };
-      
-        render() {
-          console.log("render");
-          return (
-            <div>
-              <button onClick={this.handleClick}>Click me!</button>
-              <span>{this.state.number}</span>
-            </div>
-          );
-        }
-      }
-      
-      class App extends Component {
-        render() {
-          return <ClassComp />;
-        }
-      }
+class ClassComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+  }
+
+  componentWillMount() {
+    //컴포넌트가 생성될 때 실행됩니다. (렌더링 되기 이전)
+    console.log("componentWillMount");
+  }
+
+  componentDidMount() {
+  //마운트가 완료되고 나서 실행됩니다. (렌더링 이후) 
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //상태 업데이트가 되고 컴포넌트가 업데이트 되기 전(렌더되기 전) 실행됩니다.
+    //초기 렌더링에서 발생하지 않습니다.
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    //UNSAFE_componentWillUpdate()로 변경됨, 17버전까지는 사용 가능합니다.
+    //초기 렌더링에서 발생하지 않고, 그 이후 업데이트가 되고 나서 발생합니다.
+    console.log("componetWillUpdate");
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+    //업데이트가 되고 나서 실행됩니다.
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+  //컴포넌트가 제거되기 바로 직전에 실행됩니다.
+    console.log("componentWillUnmount");
+  }
+  
+
+  handleClick = () => {
+    this.setState((state) => {
+      return {
+        number: state.number + 1,
+      };
+    });
+  };
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click me!</button>
+        <span>{this.state.number}</span>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return <ClassComp />;
+  }
+}
     `
     
     return (
@@ -165,10 +165,6 @@ export default function LifeCycle1() {
           <h2>언마운트- 컴포넌트가 사라질 때</h2>
           <p>언마운트에서는 `componentWillUnmount`가 실행됩니다. 컴포넌트를 완전히 DOM에서 제거하는 시점이다. <br />
           componentWillUnmount : 컴포넌트가 사라지기 바로 직전에 호출됩니다.</p>
-
-
-
-
         <ContentWrap>
           <CodeWrap>
             <strong>코드</strong>
