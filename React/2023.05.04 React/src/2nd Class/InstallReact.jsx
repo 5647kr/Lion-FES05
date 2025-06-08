037e088e98bbf78d0836ffe1820dd070de40d740
K's_Fe <119591236+5647kr@users.noreply.github.com>
2025-06-08 10:55:18 +0900

    InstallReact.jsx

--- React/2023.05.04 React/src/2nd Class/InstallReact.jsx
+++ React/2023.05.04 React/src/2nd Class/InstallReact.jsx
@@ -4,8 +4,11 @@ export default function InstallReact() {
   return (
     <div>
       <h1>React 설치</h1>
-      <p>React 설치 명령어</p>
-      <p>npx create-react-app 생성할폴더명 --template basic-react</p>
+      <p>React 설치 명령어 새로운 폴더 생성시</p>
+      <code>npx create-react-app 생성할폴더명 --template basic-react</code>
+
+      <p>현재 폴더에 생성시</p>
+      <code>npx create-react-app . --template basic-react</code>
     </div>
   )
 }
